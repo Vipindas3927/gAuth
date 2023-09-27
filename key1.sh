744837a463cac359ad446462f67aee3a5022da99
Vipindas K <71164644+Vipindas3927@users.noreply.github.com>
2023-09-27 11:51:03 +0530

    Add files via upload
    initial commit

---
+++ key1.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+  http://localhost:8888/google/ \
+  -H 'Content-Type: application/json' \
+    -d '{
+ "token": "ya29.a0AfB_byDjk2Im6gRPaIY8SZDuTj3UMoryFW_FfVgEO8t3sxMmGZ72LLq9-cRzAVvTPQEphR_IBkXZA3-RkVbST9d2NwIG95t1b69pIX5rRkRX_Bm0mBcmUUZaTCZInOBzW4DJBxZtbyQAhc84WOTndPaxwE53suNPFQqSaCgYKAdYSARISFQGOcNnCvxRNtD8bg4TIJq3Q3UvAXw0171"
+}'
---
+++ key2.sh
@@ -0,0 +1,3 @@
+curl -X GET \
+  http://localhost:8888/hello/ \
+  -H 'Authorization: Bearer PUT_ACCESS_TOKEN_HERE'
